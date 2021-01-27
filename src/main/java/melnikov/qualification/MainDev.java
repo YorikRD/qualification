3db086f8a38220d8134ca3b04a8e0db37a855c98
Алексей <sumkoprig@gmail.com>
2021-01-27 19:17:05 +0300

    Qualification work Java Junior. Entities  updated

---
+++ src/main/java/melnikov/qualification/MainDev.java
@@ -0,0 +1,21 @@
+package melnikov.qualification;
+
+
+import melnikov.qualification.entity.Master;
+import melnikov.qualification.entity.Player;
+
+public class MainDev {
+    public static void main(String[] args) {
+        Master player1 =  new Master ("Yorrr", "sumkoprig@gmail.com");
+        Master player2 = new Master("","saranger@inbox.ru");
+//        Master player3 = new Master(null,"sa124rangerinboxru");
+
+//        Player TruPlayer = player1.asPlayer();
+//        Player TruPlayer2 = player2.asPlayer();
+
+
+
+
+
+    }
+}
